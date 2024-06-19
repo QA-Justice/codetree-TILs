c911ce96dc0f0e7fd0c21fe9410e30c315f4fd66
Tia <136246012+QA-Justice@users.noreply.github.com>
2024-06-19 09:11:01 +0900

    Title: 1시간 뒤 시간 출력, Time: 245ms, Memory: 14MB, Status: Passed - CodeTree

--- 240619/1시간 뒤 시간 출력/print-one-hour-later.js
+++ 240619/1시간 뒤 시간 출력/print-one-hour-later.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
-let input = fs.readFileSync(0).toString().trim();
-let ab = input.split(":");
-let c = Number(ab[0]);
-let d = ab[1];
-console.log(`${c+1}:${d}`);
+let input = fs.readFileSync(0).toString().split(":");
+let hour = Number(input[0]);
+let min= Number(input[1]);
+console.log(`${hour+1}:${min}`);
