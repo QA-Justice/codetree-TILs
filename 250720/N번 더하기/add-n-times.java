fcb3678c6f85675024b2d578a15287324d328e2e
Tia <136246012+QA-Justice@users.noreply.github.com>
2025-07-20 19:36:58 +0900

    Title: N번 더하기, Time: 127ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250720/N번 더하기/add-n-times.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt(); 
+        if (a >=1 && b <=10 ){
+            int temp = a;
+        for (int i = 0; i <= a ; i++ ){
+            temp = temp+b;
+            System.out.println(temp);
+            }   
+        }
+    }
+}
