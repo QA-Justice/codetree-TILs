ceffd26e4bc1ffa53cac4cac7b3f43dfc603a703
Tia <136246012+QA-Justice@users.noreply.github.com>
2025-07-31 09:06:54 +0900

    Title: 굉장한 수, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

--- 250731/굉장한 수/amazing-number.java
+++ 250731/굉장한 수/amazing-number.java
@@ -4,7 +4,7 @@ public class Main {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        if (a%3==0||a%5==0){
+        if (a %3 == 0 && a%2 == 1||a %5 == 0 && a%2 ==0){
             System.out.println("true");
         }
         else{
