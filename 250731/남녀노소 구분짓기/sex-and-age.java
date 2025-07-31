906e7819379feefd90a4166d7cae7b781c88e33c
Tia <136246012+QA-Justice@users.noreply.github.com>
2025-07-31 09:24:25 +0900

    Title: 남녀노소 구분짓기, Time: 125ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250731/남녀노소 구분짓기/sex-and-age.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if ( a == 0 ) {
+            if ( b >= 20 ) {
+                System.out.println("MAN");
+            }
+            else {
+                System.out.println("BOY");
+            }
+        }
+        else {
+            if( b >=20 ){
+                System.out.println("WOMAN");
+            }
+            else {
+                System.out.println("GIRL");
+            }
+        }
+    }
+}
