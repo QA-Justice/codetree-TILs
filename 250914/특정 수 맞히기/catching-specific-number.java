8f7d689fe0d1ac7273a561f8cdb4ba94df200ea7
Tia <136246012+QA-Justice@users.noreply.github.com>
2025-09-14 11:44:53 +0900

    Title: 특정 수 맞히기, Time: 130ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250914/특정 수 맞히기/catching-specific-number.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        while (true) {
+            if (n == 25) {
+                System.out.println("Good");
+                break;
+            }
+            if (n < 25) {
+                System.out.println("Higher");
+            }
+            if (n > 25) {
+                System.out.println("Lower");
+            }
+            n = sc.nextInt();
+        }
+    }
+}
+
